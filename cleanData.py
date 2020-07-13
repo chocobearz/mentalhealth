f82224232f3b3c08e57f20f69136807795cda744
Paige T <ptuttosi@sfu.ca>
2020-07-13 12:15:31 -0700

    add comments

--- cleanData.py
+++ cleanData.py
@@ -1,13 +1,17 @@
 import pandas as pd
 
+#import csvs
 sadData = pd.read_csv("labeledSadPosts.csv")
 happyData = pd.read_csv("labeledHappyPosts.csv")
 
+#add the type to the id so they remain unique
 sadData['id'] = sadData['id'].apply(lambda x: "{}{}".format(x, 'sad'))
 happyData['id'] = happyData['id'].apply(lambda x: "{}{}".format(x, 'happy'))
 
+#combine the datasets
 allData = sadData.append(happyData)
 
+#remove any without entries
 allData = allData.dropna()
 
 allData.to_csv('cleanRedditPosts.csv')
