e1be55bc9b8bb5c324413c590968ee18336c9abb
Paige T <ptuttosi@sfu.ca>
2020-07-20 11:39:05 -0700

    read from a different folder

--- dataCleaning/cleanData.py
+++ dataCleaning/cleanData.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 #import csvs
-sadData = pd.read_csv("labeledSadPosts.csv")
-happyData = pd.read_csv("labeledHappyPosts.csv")
+sadData = pd.read_csv("../data/labeledSadPosts.csv")
+happyData = pd.read_csv("../data/labeledHappyPosts.csv")
 
 #add the type to the id so they remain unique
 sadData['id'] = sadData['id'].apply(lambda x: "{}{}".format(x, 'sad'))
@@ -14,4 +14,4 @@ allData = sadData.append(happyData)
 #remove any without entries
 allData = allData.dropna()
 
-allData.to_csv('cleanRedditPosts.csv')
+allData.to_csv('../data/cleanRedditPosts.csv')
