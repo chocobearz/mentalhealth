951eceb7e30009ccfafbf497e5ac2a8dc2083457
Paige T <ptuttosi@sfu.ca>
2020-07-20 11:31:54 -0700

    add all lexicon scores to afinn score csv

--- dataCleaning/afinnScore.py
+++ dataCleaning/afinnScore.py
@@ -2,8 +2,14 @@ import pandas as pd
 from afinn import Afinn
 import argparse
 
-#load afinn class
-afinn = Afinn()
+#load afinn class for base lexicon
+afinnBase = Afinn(emoticons=True)
+#load emergency lexicon
+afinnEm = Afinn('em')
+#load research lexicon
+afinnRe = Afinn('re')
+#load absolutism lexicon
+afinnAb = Afinn('ab')
 
 #allow for user to enter csv
 parser = argparse.ArgumentParser()
@@ -20,23 +26,23 @@ data = pd.read_csv(csvPath)
 
 #extract posts
 posts = list(data['text'])
-sentimentScores = []
+afinnScore = []
+researchScore = []
+emergencyScore = []
+absolutismScore = []
 
 #calculate the sentiment score for each post and keep it with the true score
 for i, post in enumerate(posts):
-  scorePair = [
-    data['id'][i],
-    data['type'][i],
-    afinn.score(post),
-    data['rating'][i]
-  ]
-  sentimentScores.append(scorePair)
+  afinnScore.append(afinnBase.score(post))
+  researchScore.append(afinnRe.score(post))
+  emergencyScore.append(afinnEm.score(post))
+  absolutismScore.append(afinnAb.score(post))
 
-print(sentimentScores)
 #store as pandas dataframe
-df = pd.DataFrame(sentimentScores)
-df.columns = ['id','type','afinnScore','rating']
+data['afinnScore'] = afinnScore
+data['researchScore'] = researchScore
+data['emergencyScore'] = emergencyScore
+data['absolutismScore'] = absolutismScore
 
-print(df)
 #save to csv
-df.to_csv('redditAfinnScores.csv')
+data.to_csv('../data/redditAfinnScores.csv')
