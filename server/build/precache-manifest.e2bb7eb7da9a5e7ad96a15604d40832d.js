d9d4a20f25c9138f011b936b2a69871d6b5616a4
aldyn <aldyn.chwelos@telus.net>
2020-07-21 14:34:28 -0700

    creating react and node apps

---
+++ server/build/precache-manifest.e2bb7eb7da9a5e7ad96a15604d40832d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea7ba5fb1fbb646387725053b2432a2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f464173dd5ddefb9416c",
+    "url": "/static/css/main.e2bab33c.chunk.css"
+  },
+  {
+    "revision": "7b2e44bd6cfecbbdcae5",
+    "url": "/static/js/2.3d55362f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.3d55362f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f464173dd5ddefb9416c",
+    "url": "/static/js/main.c5db73e8.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
