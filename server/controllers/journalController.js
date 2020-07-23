d5714234cfaf294866a1ed9eee53c5d7dcb844f9
aldyn <aldyn.chwelos@telus.net>
2020-07-23 15:55:37 -0700

    debugging

--- server/controllers/journalController.js
+++ server/controllers/journalController.js
@@ -4,7 +4,9 @@ const url = "https://jsonplaceholder.typicode.com/posts/1";
 exports.getSentimentLabel = (req, res) => {
 
     var journalEntry = req.body.journalEntry;
+    console.log("jounralEntry : " + journalEntry)
     var label = journalEntry.length % 3
+    console.log("label : " + label)
     return res.status(response.statusCode).send({
         label: label
     });
