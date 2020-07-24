49403f2edd1b869df97aa6acff680aeb588fdd72
aldyn <aldyn.chwelos@telus.net>
2020-07-23 22:39:37 -0700

    switching to using python-shell instead of child processes

--- server/controllers/journalController.js
+++ server/controllers/journalController.js
@@ -1,5 +1,5 @@
-const fetch = require("node-fetch");
 const url = "https://jsonplaceholder.typicode.com/posts/1";
+var python = require('python-shell');
 
 exports.getSentimentLabel = (req, res) => {
 
@@ -13,22 +13,28 @@ exports.getSentimentLabel = (req, res) => {
     var intercepts = "[-1.52631775, 0.20101568, 1.32530206]";
     var weights = "[[-0.02807738, 0.0393423, -0.85414827, 0.00677817],[-0.02909687, 0.03356314, -0.74487031, 0.00826826],[0.05717426, -0.07290543, 1.59901859, -0.01504643]]";
 
-    var spawn = require('child_process').spawn;
-    var py = spawn('python', ['../appScripts/predict.py',longTermScore,journalEntry,weights,intercepts]);
-    var dataString = '';
-    py.stdout.on('data', function(data) { 
+    // var spawn = require('child_process').spawn;
+    // var py = spawn('python', ['../appScripts/predict.py',longTermScore,journalEntry,weights,intercepts]);
+    // var dataString = '';
+    // py.stdout.on('data', function(data) { 
 
-        console.log(data.toString()); 
-        dataString += data
-        return res.status(200).send({
-        label: dataString
+    //     console.log(data.toString()); 
+    //     dataString += data
+    //     return res.status(200).send({
+    //     label: dataString
+    // });
+
+    var options = {
+        mode: 'text',
+        args: [longTermScore, journalEntry, weights, intercepts]
+    };
+
+    python.run('../appScripts/predict.py', options, function (err, results) { 
+      console.log(err);
+      console.log(results);
+      var res = JSON.parse(results);
+      return res.status(200).send({
+        label: res
+        });
     });
-        py.stderr.on('data', (data) => {
-   console.log(`error:${data}`);
-});
-py.stderr.on('close', () => {
-   console.log("Closed");
-});
-
-    } ) 
 };
