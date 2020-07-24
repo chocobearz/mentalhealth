ecfa61ef6008eedb46ad2a21d5953a7bb76e9e21
aldyn <aldyn.chwelos@telus.net>
2020-07-23 19:16:34 -0700

    calling predict script from node

--- appScripts/predict.py
+++ appScripts/predict.py
@@ -3,32 +3,38 @@ import argparse
 from afinnScoreApp import afinnScore
 import json
 import numpy as np
+import sys 
 
 '''predict mental state based on a journal entry'''
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-  "longTermScore",
-  help="string of journal entry"
-)
-parser.add_argument(
-  "text",
-  help="string of journal entry"
-)
-parser.add_argument(
-  "weights",
-  help="current model weights"
-)
-parser.add_argument(
-  "intercepts",
-  help="current model intercepts"
-)
-args = parser.parse_args()
+# parser = argparse.ArgumentParser()
+# parser.add_argument(
+#   "longTermScore",
+#   help="string of journal entry"
+# )
+# parser.add_argument(
+#   "text",
+#   help="string of journal entry"
+# )
+# parser.add_argument(
+#   "weights",
+#   help="current model weights"
+# )
+# parser.add_argument(
+#   "intercepts",
+#   help="current model intercepts"
+# )
+# args = parser.parse_args()
 
-longTermScore = int(args.longTermScore)
-text = args.text
-weights = np.array(json.loads(args.weights))
-intercepts = np.array(json.loads(args.intercepts))
+# longTermScore = int(args.longTermScore)
+# text = args.text
+# weights = np.array(json.loads(args.weights))
+# intercepts = np.array(json.loads(args.intercepts))
+
+longTermScore = int(sys.argv[1])
+text = sys.argv[2]
+weights = np.array(json.loads(sys.argv[3]))
+intercepts = np.array(json.loads(sys.argv[4])
 
 mod = model(weights, intercepts, longTermScore)
 
