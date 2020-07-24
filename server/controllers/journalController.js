45599ffdd55327372bff6d6a254fb006ba946db4
aldyn <aldyn.chwelos@telus.net>
2020-07-24 08:51:44 -0700

    fixing python script path

--- server/controllers/journalController.js
+++ server/controllers/journalController.js
@@ -7,48 +7,57 @@ exports.getSentimentLabel = (req, res) => {
     // var label = journalEntry.length % 3
     // console.log("label : " + label)
 
-    var longTermScore = 0;
-    var journalEntry = "Text sample"
-    var intercepts = "[-1.52631775, 0.20101568, 1.32530206]";
-    var weights = "[[-0.02807738, 0.0393423, -0.85414827, 0.00677817],[-0.02909687, 0.03356314, -0.74487031, 0.00826826],[0.05717426, -0.07290543, 1.59901859, -0.01504643]]";
+    // var longTermScore = 0;
+    // var journalEntry = "Text sample"
+    // var intercepts = "[-1.52631775, 0.20101568, 1.32530206]";
+    // var weights = "[[-0.02807738, 0.0393423, -0.85414827, 0.00677817],[-0.02909687, 0.03356314, -0.74487031, 0.00826826],[0.05717426, -0.07290543, 1.59901859, -0.01504643]]";
 
 
-    var spawn = require('child_process').spawn;
-    var py = spawn('python', ['/app/appScripts//predict.py',longTermScore,journalEntry,weights,intercepts]);
-    var dataString = '';
-    py.stdout.on('data', function(data) { 
+    // var spawn = require('child_process').spawn;
+    // var py = spawn('python', ['/app/appScripts//predict.py',longTermScore,journalEntry,weights,intercepts]);
+    // var dataString = '';
+    // py.stdout.on('data', function(data) { 
 
-        console.log(data.toString()); 
-        dataString += data
-        return res.status(200).send({
-        label: dataString
-        });
-    })
-    //runPy(res)
+    //     console.log(data.toString()); 
+    //     dataString += data
+    //     return res.status(200).send({
+    //     label: dataString
+    //     });
+    // })
+    console.log("about to runpy")
+    runPy(res)
 
 };
 
 
-// const runPy = async (res) => {
-//         var {PythonShell} = require('python-shell') 
-//     var longTermScore = 0;
-//     var journalEntry = "Text sample"
-//     var intercepts = "[-1.52631775, 0.20101568, 1.32530206]";
-//     var weights = "[[-0.02807738, 0.0393423, -0.85414827, 0.00677817],[-0.02909687, 0.03356314, -0.74487031, 0.00826826],[0.05717426, -0.07290543, 1.59901859, -0.01504643]]";
-//         var options = {
-//         mode: 'text',
-//         args: [longTermScore, journalEntry, weights, intercepts],
-//         scriptPath: '/app/appScripts/'
-//         };
+const runPy = async (res) => {
+    var {PythonShell} = require('python-shell') 
+    var longTermScore = 0;
+    var journalEntry = "Text sample"
+    var intercepts = "[-1.52631775, 0.20101568, 1.32530206]";
+    var weights = "[[-0.02807738, 0.0393423, -0.85414827, 0.00677817],[-0.02909687, 0.03356314, -0.74487031, 0.00826826],[0.05717426, -0.07290543, 1.59901859, -0.01504643]]";
+        var options = {
+        mode: 'text',
+        args: [longTermScore, journalEntry, weights, intercepts],
+        scriptPath: '/app/appScripts/'
+        };
 
-//         const result = await new Promise((resolve, reject) => {
-//             PythonShell.run('predict.py', options, (err, results) => {
-//             if (err) return reject(err);
-//             return resolve(results);
-//             });
-//         });
-//         console.log(result.stdout);
-//         return res.status(200).send({
-//             label: results
-//         });
-// };
+        PythonShell.run('predict.py', options, (err, results) => {
+            if (err) console.log(err);
+            console.log(results)
+            return res.status(200).send({
+                label: results
+            });
+        });
+
+        // const result = await new Promise((resolve, reject) => {
+        //     PythonShell.run('predict.py', options, (err, results) => {
+        //     if (err) return reject(err);
+        //     return resolve(results);
+        //     });
+        // });
+        // console.log(result.stdout);
+        // return res.status(200).send({
+        //     label: results
+        // });
+};
