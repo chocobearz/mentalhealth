559ab5da73493d75b03645055fa2e567effbc6e8
aldyn <aldyn.chwelos@telus.net>
2020-07-24 20:03:50 -0700

    pulling weight + intercepts from db

--- server/controllers/journalController.js
+++ server/controllers/journalController.js
@@ -12,17 +12,25 @@ exports.getSentimentLabel = (req, res) => {
         port: 5432,
     });
 
+    let weights;
+    let intercepts;
+
     client.connect();
 
     const query = `
         SELECT *
         FROM users
+        WHERE user_id = 1
         `;
 
     client
     .query(query)
-    .then(res => {
-        console.log(res);
+    .then(dbResponse => {
+        console.log(dbResponse)
+        weights = dbResponse[0].weights
+        intercepts = dbResponse[0].intercepts
+        var journalEntry = req.body.journalEntry;
+        runPredict(res, journalEntry, weights, intercepts)
     })
     .catch(err => {
         console.error(err);
@@ -31,18 +39,15 @@ exports.getSentimentLabel = (req, res) => {
         client.end();
     });
 
-    var journalEntry = req.body.journalEntry;
-    runPredict(res, journalEntry)
-
 };
 
 
-const runPredict = async (res, journalEntry) => {
+const runPredict = async (res, journalEntry, weights, intercepts) => {
     var {PythonShell} = require('python-shell') 
     var longTermScore = 0;
     //var journalEntry = "Text sample"
-    var intercepts = "[-1.52631775, 0.20101568, 1.32530206]";
-    var weights = "[[-0.02807738, 0.0393423, -0.85414827, 0.00677817],[-0.02909687, 0.03356314, -0.74487031, 0.00826826],[0.05717426, -0.07290543, 1.59901859, -0.01504643]]";
+    //var intercepts = "[-1.52631775, 0.20101568, 1.32530206]";
+    //var weights = "[[-0.02807738, 0.0393423, -0.85414827, 0.00677817],[-0.02909687, 0.03356314, -0.74487031, 0.00826826],[0.05717426, -0.07290543, 1.59901859, -0.01504643]]";
         var options = {
         mode: 'text',
         args: [longTermScore, journalEntry, weights, intercepts],
