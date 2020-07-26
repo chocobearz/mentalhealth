5049576fbe16286473775771932b156c3bbaea11
aldyn <aldyn.chwelos@telus.net>
2020-07-25 20:33:43 -0700

    debugging

--- server/controllers/journalController.js
+++ server/controllers/journalController.js
@@ -35,7 +35,7 @@ exports.getSentimentLabel = (req, res) => {
         weights = dbResponse.rows[0].weights
         intercepts = dbResponse.rows[0].intercepts
         var journalEntry = req.body.journalEntry;
-        const ratings =  runPredict(res, journalEntry, weights, intercepts);
+        const ratings =  await runPredict(res, journalEntry, weights, intercepts);
         const values = [ratings.longTermScore, ratings.currentRating]
         client
         .query(query2Text, values)
@@ -53,23 +53,26 @@ exports.getSentimentLabel = (req, res) => {
 };
 
 
-const runPredict =  (res, journalEntry, weights, intercepts) => {
-    var {PythonShell} = require('python-shell') 
-    var longTermScore = 0;
-    //var journalEntry = "Text sample"
-    //var intercepts = "[-1.52631775, 0.20101568, 1.32530206]";
-    //var weights = "[[-0.02807738, 0.0393423, -0.85414827, 0.00677817],[-0.02909687, 0.03356314, -0.74487031, 0.00826826],[0.05717426, -0.07290543, 1.59901859, -0.01504643]]";
-    var options = {
-        mode: 'text',
-        args: [longTermScore, journalEntry, weights, intercepts],
-        scriptPath: '/app/appScripts/'
-    };
+const runPredict =  async (res, journalEntry, weights, intercepts) => {
+    return  Promise((resolve, reject) => {
+        var {PythonShell} = require('python-shell') 
+        var longTermScore = 0;
+        //var journalEntry = "Text sample"
+        //var intercepts = "[-1.52631775, 0.20101568, 1.32530206]";
+        //var weights = "[[-0.02807738, 0.0393423, -0.85414827, 0.00677817],[-0.02909687, 0.03356314, -0.74487031, 0.00826826],[0.05717426, -0.07290543, 1.59901859, -0.01504643]]";
+        var options = {
+            mode: 'text',
+            args: [longTermScore, journalEntry, weights, intercepts],
+            scriptPath: '/app/appScripts/'
+        };
 
-    PythonShell.run('predict.py', options, (err, results) => {
-        if (err) console.log(err);
-        return {
-            currentRating: results[0],
-            longTermScore: results[1]
-        }
-    });
+        PythonShell.run('predict.py', options, (err, results) => {
+            if (err) console.log(err);
+            resolve( {
+                currentRating: results[0],
+                longTermScore: results[1]
+            })
+        });
+    })
+
 };
